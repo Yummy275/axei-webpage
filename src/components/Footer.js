78f6e9a8923ca00e08d1b0b1837cba723c5205c0
Yummy275 <htgonzalez94@yahoo.com>
2021-06-09 14:59:18 -0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,33 @@ const Container = styled.div`
     height: 10rem;
     margin-top: 15rem;
     background-image: linear-gradient(135deg, #72edf2 10%, #5151e5 100%);
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    padding: 1rem;
 `;
 
-const SmlTxt = styled.p``;
+const Title = styled.h2`
+    text-align: center;
+    margin-bottom: 1rem;
+`;
+const Link = styled.a`
+    margin-bottom: 0.5rem;
+    text-decoration: underline;
+    cursor: pointer;
+    color: white;
+`;
 
 const Footer = () => {
     return (
         <Container>
-            <SmlTxt>Footer</SmlTxt>
+            <Title>Mock site made by Humberto Gonzalez.</Title>
+            <Link href="https://yummy275.github.io/hg-portfolio/">
+                Visit My Portfolio Page
+            </Link>
+            <Link href="https://github.com/Yummy275">Visit My Github</Link>
         </Container>
     );
 };
