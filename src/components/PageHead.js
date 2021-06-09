d9bc00b48f98914e15cfe74032a5ca0efdd40726
Yummy275 <htgonzalez94@yahoo.com>
2021-06-08 20:59:22 -0700

    head looks ok

---
+++ src/components/PageHead.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-image: linear-gradient(135deg, #72edf2 10%, #5151e5 100%);
+    color: white;
+`;
+
+const BackgroundImgDiv = styled.div`
+    text-align: center;
+    font-size: 2rem;
+`;
+
+const Title = styled.h1``;
+
+const SubTitle = styled.h3``;
+const PageHead = () => {
+    return (
+        <Container>
+            <BackgroundImgDiv>
+                <Title>AXEI</Title>
+                <SubTitle>Software Development</SubTitle>
+            </BackgroundImgDiv>
+        </Container>
+    );
+};
+
+export default PageHead;
